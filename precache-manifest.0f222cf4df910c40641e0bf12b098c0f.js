a548429d270d741ca08014151a562304226f1bdb
Kashishdwn <kashishdwn@gmail.com>
2020-05-10 01:27:58 +0530

    Updates

---
+++ precache-manifest.0f222cf4df910c40641e0bf12b098c0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc584ffab708c7d5dbdb3ccb5063d19",
+    "url": "/Color-Box-Maker/index.html"
+  },
+  {
+    "revision": "3e37f112b33d786420b2",
+    "url": "/Color-Box-Maker/static/css/main.e19581ef.chunk.css"
+  },
+  {
+    "revision": "344bf2e94d93aff37d50",
+    "url": "/Color-Box-Maker/static/js/2.95e222b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Color-Box-Maker/static/js/2.95e222b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e37f112b33d786420b2",
+    "url": "/Color-Box-Maker/static/js/main.216bc081.chunk.js"
+  },
+  {
+    "revision": "93d09a8408abed362443",
+    "url": "/Color-Box-Maker/static/js/runtime-main.471615af.js"
+  }
+]);
